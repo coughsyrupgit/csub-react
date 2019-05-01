ae451d5ba7ed4e8a314ae1fda6c081c78fe46442
Alex <alexander.galdin92@gmail.com>
2019-05-01 03:34:17 +0300

    Basic logic

--- src/App.js
+++ src/App.js
@@ -4,36 +4,56 @@ import 'uikit/dist/css/uikit-core.css'
 import Grid from './components/Grid'
 import { logger } from 'handlebars';
 
-function App() {
-  var test = [
-      {
-          url: "#",
-          title: "Title 1"
-      },
-      {
-          url: "#",
-          title: "Link 2"
-      }
-  ]
+const bookmarks = window.chrome.bookmarks;
 
-  return (
-      <div className="uk-container uk-container-large">
-          <h1 className="uk-heading-line uk-margin-top uk-margin-medium-bottom"><span>My Bookmarks</span></h1>
-          <Grid items={[{
-              title: "Card 1",
-              links: test
-          },
-          {
-              title: "Card 2",
-              links: test
-          },
-          {
-              title: "Card 3",
-              links: test
-          }
-          ]} />
-      </div>
-  )
+const FolderObject = function (treeNode) {
+    let links = treeNode.children.filter(function (item) {
+        return !item.children
+    });
+
+    return {
+        id: treeNode.id,
+        title: treeNode.title,
+        links: links
+    }
+}
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        bookmarks.getTree(this.prepareFolders.bind(this));
+
+        this.state = {
+            tree : [],
+            folders: []
+        }
+    }
+
+    prepareFolders(tree) {
+        var self = this;
+
+        tree.forEach((subtree) => {
+            if (subtree.children) {
+                if (subtree.children.filter((item) => !item.children).length) {
+                    let folders = [].concat(self.state.folders);
+                    folders.push(new FolderObject(subtree));
+                    self.setState(Object.assign(self.state, {
+                        folders: folders
+                    }))
+                }
+                self.prepareFolders(subtree.children)
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="uk-container uk-container-large">
+                <h1 className="uk-heading-line uk-margin-top uk-margin-medium-bottom"><span>My Bookmarks</span></h1>
+                <Grid items={this.state.folders} />
+            </div>
+        )
+    }
 }
 
 export default App;
